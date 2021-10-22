c1e40d7dfe46100608acba1faffa57449f025a1c
Vsevolod Kondratev <vsevolod.kondratev@gmail.com>
2021-10-22 14:31:09 +0700

    Initial Commit

---
+++ HW2.2.1_sliderColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.2.1_sliderColor
+//
+//  Created by Всеволод Кондратьев on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
